3084ccf90970dbbcf9001c482eca3c5e4e4e326c
JOMADELEMA <jomadelema@gmail.com>
2022-10-04 23:06:22 -0600

    first commit, starting the project

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html", "./location.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
